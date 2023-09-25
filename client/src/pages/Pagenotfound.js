d82959bad6bb9bb1d26924d7811537eb219f86e4
SnehaPoudel <sneha.deerwalk.edu.np>
2023-09-25 14:00:57 +0545

    Update dependencies, add react-icons, add footer, add 404 page

--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
+import { PiNutFill } from "./../../node_modules/react-icons/pi/index.esm";
 
 const Pagenotfound = () => {
   return (
     <Layout>
-      <h1>Page not Found</h1>
+      <div className="pnf">
+        <h1 className="pnf-title">404</h1>
+        <h2 className="pnf-heading">Oops ! Page Not Found</h2>
+        <Link to="/" className="pnf-btn">
+          Go Back
+        </Link>
+      </div>
     </Layout>
   );
 };
