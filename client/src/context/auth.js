fafd5b7c1fd756d7dfa8c030c910bc5d731553fc
SnehaPoudel <sneha.deerwalk.edu.np>
2023-09-26 19:39:19 +0545

    Update login page and add protected route

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext, createContext } from "react";
-
+import axios from "axios";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
@@ -7,6 +7,9 @@ const AuthProvider = ({ children }) => {
     user: null,
     token: "",
   });
+  //default axios
+  axios.defaults.headers.common["Authorization"] = auth?.token;
+
   useEffect(() => {
     const data = localStorage.getItem("auth");
     if (data) {
@@ -17,6 +20,7 @@ const AuthProvider = ({ children }) => {
         token: parseData.token,
       });
     }
+    //eslint-disable-next-line
   }, [auth]);
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
